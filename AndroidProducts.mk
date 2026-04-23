49c6eaa298163c1b12a2fe2f3acbbf6af2425042
unik2002 <unikkinzy1@gmail.com>
2026-04-23 11:11:33 +0000

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_Infinix-X669D.mk
+    $(LOCAL_DIR)/twrp_X669D.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_Infinix-X669D-user \
-    omni_Infinix-X669D-userdebug \
-    omni_Infinix-X669D-eng
+    twrp_X669D-user \
+    twrp_X669D-userdebug \
+    twrp_X669D-eng

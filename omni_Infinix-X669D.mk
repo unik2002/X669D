9c248b5348b0f7e3e3cfe488d92a47554fa18559
unik2002 <unikkinzy1@gmail.com>
2026-04-23 11:08:11 +0000

    Update omni_Infinix-X669D.mk

--- omni_Infinix-X669D.mk
+++ omni_Infinix-X669D.mk
@@ -5,25 +5,36 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+DEVICE_PATH := device/infinix/X669D
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Inherit from Infinix-X669D device
-$(call inherit-product, device/infinix/Infinix-X669D/device.mk)
+# Inherit common TWRP configuration (Matches the TWRP build environment)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
-PRODUCT_DEVICE := Infinix-X669D
-PRODUCT_NAME := omni_Infinix-X669D
-PRODUCT_BRAND := Infinix
-PRODUCT_MODEL := Infinix X669D
+# Inherit from the device-specific makefile
+$(call inherit-product, device/infinix/X669D/device.mk)
+
+# Device Identity
+PRODUCT_DEVICE := X669D
+PRODUCT_NAME := twrp_X669D
+PRODUCT_BRAND := infinix
+PRODUCT_MODEL := infinix X669D
 PRODUCT_MANUFACTURER := infinix
+PRODUCT_RELEASE_NAME := infinix infinix X669D
 
+# Client ID for Transsion/Infinix
 PRODUCT_GMS_CLIENTID_BASE := android-transsion
 
+# Build Properties Overrides
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="X669D-user 12 SP1A.210812.016 89 release-keys"
 
+# Using the more recent fingerprint (2025 version)
 BUILD_FINGERPRINT := Infinix/X669D-GL/Infinix-X669D:12/SP1A.210812.016/GL-20250207V440:user/release-keys
